7688d0f9983eb33260e06e8677511ec9e4b01ab0
Ken Collins <ken@metaskills.net>
2024-12-06 21:57:38 -0500

    Start off zep4

---
+++ src/zep4.ts
@@ -0,0 +1,63 @@
+import { streamText } from "ai";
+import { inquire } from "./shared/inquire.js";
+import { model } from "./shared/models.js";
+
+import {
+  zep,
+  getOrAddUser,
+  getOrAddSession,
+  getMessages,
+} from "./shared/zep.js";
+
+async function chat(newUserMessage: string) {
+  const { memory, messages }: any = await getMessages(
+    sessionId,
+    newUserMessage
+  );
+  const timestamp = new Date().toISOString().replace("T", " ").split(".")[0];
+  const stream = streamText({
+    model: model,
+    system: `
+You are a helpful assistant. Use the following private facts and entities, which include timestamps, to inform your responses. Utilize the timestamps to ensure information is accurate and relevant, but do not mention or reference the facts, entities, or timestamps to the user in any way.
+
+${memory.context}
+
+The current time is ${timestamp}.`.trim(),
+    messages: messages,
+    temperature: 0.1,
+    maxTokens: 1000,
+  });
+  let fullResponse = "";
+  for await (const chunk of stream.textStream) {
+    process.stdout.write(chunk);
+    fullResponse += chunk;
+  }
+  process.stdout.write("\n\n");
+  await zep.memory.add(sessionId, {
+    messages: [
+      { roleType: "user", content: newUserMessage },
+      { roleType: "assistant", content: fullResponse },
+    ],
+  });
+}
+
+const userId = "1";
+const sessionId = "1";
+
+await getOrAddUser({
+  userId: userId,
+  email: "ken@unremarkable.ai",
+  firstName: "Ken",
+  lastName: "Collins",
+});
+
+await getOrAddSession({
+  sessionId: sessionId,
+  userId: userId,
+});
+
+while (true) {
+  const newUserMessage = await inquire();
+  if (newUserMessage.toLowerCase() === "exit") break;
+  await chat(newUserMessage);
+}
